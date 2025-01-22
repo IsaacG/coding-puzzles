b35a900bc07a1215fd4a8f1b056553520b4c4255
Isaac Good <github@isaacgood.com>
2025-01-21 21:55:25 -0800

    IsaacG solution for practice 01

---
+++ CodingQuest.io/2022-practice-01/IsaacG.py
@@ -0,0 +1,41 @@
+#!/bin/python
+
+"""CodingQuest.io 2022 Practice 01: Snakes and Ladders.
+
+https://codingquest.io/problem/13
+"""
+
+import os
+import pathlib
+
+
+def solve(data: str) -> int:
+    """Solve which player wins and when."""
+    board_data = [line for line in data.splitlines() if len(line.split()) > 2]
+    dice_data = [line for line in data.splitlines() if len(line.split()) == 2]
+    board = []
+    for idx, line in enumerate(reversed(board_data)):
+        nums = [int(i) for i in line.split()]
+        if idx % 2:
+            nums.reverse()
+        board.extend(nums)
+    end = len(board) - 1
+
+    position = {1: 0, 2: 0}
+    for moves, line in enumerate(dice_data, start=1):
+        for player, roll in enumerate((int(i) for i in line.split()), start=1):
+            while roll != 0:
+                position[player] += roll
+                if position[player] >= end:
+                    return player * moves
+                roll = board[position[player]]
+
+
+if __name__ == "__main__":
+    paths = [
+        pathlib.Path(__file__).parent / "example_data",
+        pathlib.Path(os.getenv("HOME")) / "coding_quest_inputs" / "13.txt",
+    ]
+    for path in paths:
+        data = path.read_text()
+        print(solve(data))
